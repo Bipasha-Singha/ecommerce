43fb5654bbf26346cb635782b7c46af0f21185b4
Bipasha-Singha <63922620+Bipasha-Singha@users.noreply.github.com>
2023-05-11 05:22:32 -0700

    Update cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,28 +1,56 @@
 const Cart = require('../models/cart');
 const User = require('../models/usermodel');
 const Product = require('../models/productmodel');
-
 const addToCart = async (req, res) => {
-  try{
-    // Check if the user is authenticated
-    if (!req.user) {
-      return res.status(401).send({ success: false, msg: "Please login to access this resource" });
+  try {
+    const userId = req.params.userId;
+
+    // Find the product and update its quantity in the Product collection
+    const product = await Product.findById(req.body.product);
+
+    if (!product) {
+      return res.status(400).send({ success: false, msg: "Product not found" });
     }
-     // Find the product and update its quantity in the Product collection
-     const product = await Product.findById(req.body.product);
-     product.quantity -= req.body.quantity;
-     await product.save();
+
+   const desiredQuantity = req.body.quantity;
+    if (product.quantity < desiredQuantity) {
+      return res.status(422).send({ success: false, msg: `Only ${product.quantity} item(s) available` });
+    }
+
+    product.quantity -= desiredQuantity;
+    if (product.quantity < 1) {
+      return res.status(400).send({ success: false, msg: "Product is out of stock" });
+    }
+    
+    await product.save();
+
+    // Get the price of the product
+    const price = product.price * desiredQuantity;
 
     const cart_obj = new Cart({
       product: req.body.product,
-      quantity: req.body.quantity,
-      user: req.user._id // Set the user ID to the current authenticated user
+      quantity: desiredQuantity,
+      price: price,
+      user: userId 
     });
+
     const cartData = await cart_obj.save();
 
     res.status(200).send({success:true, msg:"Cart Product Details", data:cartData});
-  }catch (error){
+  } catch (error) {
     res.status(400).send({success:false, msg: error.message});
   }
 }
-module.exports = { addToCart };
+const getCartItems = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    // Find the cart items for the specified user
+    const cartItems = await Cart.find({ user: userId }).populate('product');
+
+    res.status(200).send({ success: true, msg: "Cart items retrieved", data: cartItems });
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error.message });
+  }
+}
+module.exports = { addToCart, getCartItems };
