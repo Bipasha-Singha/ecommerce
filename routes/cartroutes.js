532b543d705d8650b554c1bc0aa6bcad6d81b1d6
Bipasha-Singha <63922620+Bipasha-Singha@users.noreply.github.com>
2023-05-18 13:09:40 -0700

    Update cartroutes.js

--- routes/cartroutes.js
+++ routes/cartroutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const{addToCart}= require("../controllers/cartController");
-const { isauthenticatedUser} = require("../middlewares/auth");
+const Cart = require('../models/cart');
+const { addToCart, getCartItems, removeCartItem } = require("../controllers/cartController");
+//const { isauthenticatedUser } = require("../middlewares/auth");
 const router = express.Router();
-//const(isauthenticatedUser)= require("../middlewares/auth");
 
-router.route("/cart/new").post(isauthenticatedUser, addToCart);
-
+router.route("/cart/:userId").post(addToCart);
+router.route("/cart/:userId").get(getCartItems);
+router.route("/cart/:userId/:cartItemId").delete(removeCartItem);
 module.exports = router;
