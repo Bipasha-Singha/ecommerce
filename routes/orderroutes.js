510c73372fd117736c9065e2fdf41b25aa5a92d0
Bipasha-Singha <63922620+Bipasha-Singha@users.noreply.github.com>
2023-05-18 13:11:10 -0700

    Delete orderroutes.js

--- routes/orderroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const{newOrder}= require("../controllers/ordercontroller");
-const { isauthenticatedUser} = require("../middlewares/auth");
-const router = express.Router();
-//const(isauthenticatedUser)= require("../middlewares/auth");
-
-router.route("/order/new").post(isauthenticatedUser, newOrder);
-
-module.exports = router;
